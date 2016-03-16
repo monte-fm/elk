e96c2169ac86854ad7ecd2611a2137e14d12dc78
Olexander Kutsenko <monte-fm@users.noreply.github.com>
2016-03-16 18:21:58 +0200

    git config --global

--- configs/etckeeper.sh
+++ configs/etckeeper.sh
@@ -32,6 +32,9 @@ if [ -d /root/etckeeper ] ; then
 fi
 " >> ~/.bashrc
 
+git config --global user.email "you@example.com"
+git config --global user.name "ELK user"
+
 cd /etc
 etckeeper init
 git add .
